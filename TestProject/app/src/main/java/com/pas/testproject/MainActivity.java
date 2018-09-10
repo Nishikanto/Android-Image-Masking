57b541716059a57b05db355043cbdf642bd5593c
nishikanto <nishikanto.abm@gmail.com>
2018-09-10 18:37:48 +0600

    initial push

---
+++ TestProject/app/src/main/java/com/pas/testproject/MainActivity.java
@@ -0,0 +1,69 @@
+package com.pas.testproject;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Glide.with(this)
+                .asBitmap()
+                .load("https://i.ytimg.com/vi/teOV1JYid50/mqdefault.jpg")
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+
+                        ImageView mImageView= (ImageView)findViewById(R.id.orginal_imageView);
+                        Bitmap mask = BitmapFactory.decodeResource(getResources(),R.drawable.mask);
+                        Bitmap original = getResizedBitmap(resource, mask.getWidth(), mask.getHeight());
+
+                        //You can change original image here and draw anything you want to be masked on it.
+
+                        Bitmap result = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
+                        Canvas tempCanvas = new Canvas(result);
+                        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+                        tempCanvas.drawBitmap(original, 0, 0, null);
+                        tempCanvas.drawBitmap(mask, 0, 0, paint);
+                        paint.setXfermode(null);
+
+                        //Draw result after performing masking
+                        mImageView.setImageBitmap(result);
+                    }
+                });
+    }
+
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+}
